528f9f37b0544f9592f055605a9ccf013d15a5c2
SimbanaPablo <pablo.simbana01@gmail.com>
2024-02-19 21:05:59 -0500

    taller35

--- src/main/java/com/uce/edu/ec/Pa2U3P5PsApplication.java
+++ src/main/java/com/uce/edu/ec/Pa2U3P5PsApplication.java
@@ -1,7 +1,10 @@
 package com.uce.edu.ec;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.hibernate.internal.build.AllowSysOut;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -33,13 +36,55 @@ public class Pa2U3P5PsApplication implements CommandLineRunner {
 	public static void main(String[] args) {
 		SpringApplication.run(Pa2U3P5PsApplication.class, args);
 	}
- 
+
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
-		//this.facturaService.pruebaSupports();
-		this.facturaService.pruebaNever();
+		// this.facturaService.pruebaSupports();
+		// this.facturaService.pruebaNever();
+		/*
+		 * System.out.println("Nombre hilo: "+Thread.currentThread().getName()); long
+		 * tiempoInicial = System.currentTimeMillis(); for(int i=1;i<=10;i++) { Cliente
+		 * cliente = new Cliente(); cliente.setNombre("CN"+i);
+		 * cliente.setApellido("CA"+i); this.clienteService.guardar(cliente); } long
+		 * tiempoFinal = System.currentTimeMillis();
+		 * 
+		 * long tiempoTranscurrido = (tiempoFinal - tiempoInicial)/1000;
+		 * System.out.println("Tiempo transcurrido: "+tiempoTranscurrido); //Tiempo
+		 * transcurrido en seg:101
+		 */
+		// Programcion en paralelo (multihilo - MultiThread)
+//		System.out.println("Nombre hilo: "+Thread.currentThread().getName());
+//		long tiempoInicial = System.currentTimeMillis();
+//		List<Cliente> listaCliente = new ArrayList<>();
+//		for (int i = 1; i <= 100; i++) {
+//			Cliente cliente = new Cliente();
+//			cliente.setNombre("CN" + i);
+//			cliente.setApellido("CA" + i);
+//			listaCliente.add(cliente);
+//		}
+//		listaCliente.stream().forEach(cliente -> this.clienteService.guardar(cliente));
+//		long tiempoFinal = System.currentTimeMillis();
+//
+//		long tiempoTranscurrido = (tiempoFinal - tiempoInicial) / 1000;
+//		System.out.println("Tiempo transcurrido: " + tiempoTranscurrido);
+		// Tiempo transcurrido en seg:101
+		
+		System.out.println("Nombre hilo: "+Thread.currentThread().getName());
+		long tiempoInicial = System.currentTimeMillis();
+		List<Cliente> listaCliente = new ArrayList<>();
+		for (int i = 1; i <= 100; i++) {
+			Cliente cliente = new Cliente();
+			cliente.setNombre("CN" + i);
+			cliente.setApellido("CA" + i);
+			listaCliente.add(cliente);
+		}
+		listaCliente.parallelStream().forEach(cliente -> this.clienteService.guardar(cliente));
+		long tiempoFinal = System.currentTimeMillis();
 
+		long tiempoTranscurrido = (tiempoFinal - tiempoInicial) / 1000;
+		System.out.println("Tiempo transcurrido: " + tiempoTranscurrido);
+		//Tiempo transcurrido en seg:25
 	}
 
 }
--- src/main/java/com/uce/edu/ec/ventas/service/ClienteServiceImpl.java
+++ src/main/java/com/uce/edu/ec/ventas/service/ClienteServiceImpl.java
@@ -1,5 +1,7 @@
 package com.uce.edu.ec.ventas.service;
 
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
@@ -20,11 +22,16 @@ public class ClienteServiceImpl implements IClienteService {
 	@Transactional(value = TxType.REQUIRES_NEW)
 	public void guardar(Cliente cliente) {
 		// TODO Auto-generated method stub
+
+		System.out.println("Nombre hilo: "+Thread.currentThread().getName());
+		this.clienteRepository.insertar(cliente);
 		try {
-			this.clienteRepository.insertar(cliente);
-		} catch (Exception e) {
-			// TODO: handle exception
+			TimeUnit.SECONDS.sleep(1);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+		
 	}
 
 	@Override
